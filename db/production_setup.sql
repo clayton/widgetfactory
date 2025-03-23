494bacfdd39b1443de3d77b12b84ebc4546763c7
Clayton Lengel-Zigich <6334+clayton@users.noreply.github.com>
2025-03-22 23:32:59 -0700

    postgres

---
+++ db/production_setup.sql
@@ -0,0 +1,6 @@
+-- db/production_setup.sql
+ 
+CREATE DATABASE widgetfactory_production;
+CREATE DATABASE widgetfactory_production_cache;
+CREATE DATABASE widgetfactory_production_queue;
+CREATE DATABASE widgetfactory_production_cable;
